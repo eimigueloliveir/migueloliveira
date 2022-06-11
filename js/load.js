ccea47bad08299a9aa2af34718ae8daacea9ffa7
Miguel Oliveira <migueloliveiradasneves@gmail.com>
2022-06-10 23:56:32 -0300

    updates

--- js/load.js
+++
@@ -1,7 +0,0 @@
-var load = document.getElementById("loading");
-function loadfun()
-{
-    load.style.visibility = "hidden"
-    load.style.opacity = 0
-    load.style.transition = "visibility 0s 1s, opacity 1s linear";
-}
