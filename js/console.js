cb455416a17948a3e6dfc4c9da55c253eca4bffe
Miguel Oliveira <migueloliveiradasneves@gmail.com>
2022-06-11 15:56:41 -0300

    Co-authored-by: Paulo Henrique Pereira Pires da Silva <paulohpps@users.noreply.github.com>

---
+++ js/console.js
@@ -0,0 +1,40 @@
+const inputEle = document.getElementById('enter');
+    const consoleEle = document.getElementById('console-text');
+    inputEle.addEventListener('keyup', function(e){
+    var key = e.which || e.keyCode;
+    if (key == 13) { 
+      var texto = inputEle.value;
+      texto = texto.trim();
+      if(texto == "dotnet"){
+      consoleEle.innerHTML += '<br><span id="console-tex">' + "<br>Options:<br>Usage: dotnet run<br>Usage: dotnet --version" + '</span><br>';
+      }
+      else if(texto == "dotnet run"){
+      consoleEle.innerHTML += '<span id="console-tex">' + "I'm Miguel Oliveira, I'm 16 years old, I like programming, games, movies, among other things..." + '</span><br>';
+      const text = document.getElementById('console-tex');
+      typeWriter(text);
+      }
+      else if(texto == "dotnet --version"){
+      consoleEle.innerHTML += '<br><span id="console-tex">' + "6.0.300" + '</span><br>';
+      const text = document.getElementById('console-tex');
+      typeWriter(text);
+      }
+      else if(texto == "")
+      {
+
+      }
+      else{
+          consoleEle.innerHTML += '<br><span id="console-tex"> ' + inputEle.value + " não é reconhecido como um comando interno</span><br>"+
+          "<span id='console-tex'> ou externo, um programa operável ou um arquivo em lotes.</span><br>";
+      }
+      
+    }
+  });
+
+  function typeWriter(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    for(let i = 0; i < textoArray.length; i++) {
+        setTimeout(() => elemento.innerHTML += textoArray[i], 75 * i);
+     }
+    }
+  
